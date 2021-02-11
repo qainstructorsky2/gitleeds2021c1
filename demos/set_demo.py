db085177876f9e3ac3a4caefcf788702b76b4faf
QAInstructorSky2 <instructorsky2@gmail.com>
2021-02-11 19:30:08 +0000

    new stuf

---
+++ demos/set_demo.py
@@ -0,0 +1,14 @@
+mydict = {'UK': ['London', 'Wigan', 'Macclesfield', 'Bolton'],
+
+          'US': ['Miami', 'Springfield', 'New York', 'Boston']}
+
+print(mydict['UK'][2])
+
+homer = 1
+
+print(mydict['US'][homer])
+
+mydict['FR'] = ['Paris', 'Lyon', 'Bordeaux', 'Toulouse']
+
+for country in mydict.keys():
+    print(country, ': ', mydict[country])
