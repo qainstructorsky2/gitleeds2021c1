c3d5976d10b77f5737b54c0cfadbae239c2980cd
QAInstructorSky2 <instructorsky2@gmail.com>
2021-02-24 21:40:42 +0000

    Sample RSP

--- demos/funnctions.py
+++ demos/funnctions.py
@@ -15,5 +15,6 @@ hello(pet3="Meg",  pet1="Lex")
 
 
 
-
+addItem = lambda itemone : itemone  +  45
+print(addItem(5))
 
--- demos/lambda.py
+++ demos/lambda.py
@@ -1,6 +1,14 @@
-#lambda
-addItem = lambda itemone : itemone  +  45
-print(addItem(5))
+choices = { "R":,Rock "P": Paper, "S: Scissors" }
 
-def addItem(itemone):
-    return itemone + 45
+# Get the user's choice of the three
+user_choice = input("\nType R, P, or S to play against the computer: ")
+
+# Print what the user's choice, so she knows what she picked
+print user_choice
+
+# Make sure the user input is valid
+if user_choice in choices:
+   print "You chose %s." % user_choice
+else:
+   print "Your choice of %s is not a valid choice." % user_choice
+
